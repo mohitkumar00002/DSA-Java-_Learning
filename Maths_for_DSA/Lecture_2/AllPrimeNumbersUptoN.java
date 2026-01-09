de547b3d7a78f57fa31a5cc1ab8c49b5402cf07a
Mohit Kumar <rajputmohit0002@gmail.com>
2026-01-09 09:53:53 +0530

    Finding All Prime numbers Upto N

--- Maths_for_DSA/Lecture_2/AllPrimeNumbersUptoN.java
+++ Maths_for_DSA/Lecture_2/AllPrimeNumbersUptoN.java
@@ -5,14 +5,22 @@ public class AllPrimeNumbersUptoN {
     Scanner input = new Scanner(System.in);
     System.out.print("Enter the number: ");
     int num = input.nextInt();
-  }
-  static int primeNum(int num){
-    if(num<=1) return -1;
-    if(num == 2) return 1;
-    if(num % 2 == 0) return -1;
-    int count = 1;
-    for(int i=3; i*i<=num;i+=2){
-      
+    
+    for(int j=2;j<=num;j++){
+      if(isPrime(j)){
+        System.out.print(j + " ");
+      }
     }
   }
+  static boolean isPrime(int num){
+    if(num <= 1) return false;
+    if(num == 2) return true;
+    if(num % 2 == 0) return false;
+    
+    for(int i=3;i*i<=num; i+=2){
+      if(num % i==0) return false;
+    }
+    return true;
+  }
+  
 }
---
+++ Maths_for_DSA/Lecture_2/BinarySearchSQRT.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class BinarySearchSQRT {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the number: ");
+    int num = input.nextInt();
+
+    System.out.print("Enter the precision value: ");
+    int precision = input.nextInt();
+
+    System.out.println(sqrt(num, precision));
+  }
+
+  static double sqrt(int n, int p) {
+    int start = 0;
+    int end = n;
+    double root = 0.0;
+
+    // Binary Search for integer part
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+
+      if ((long) mid * mid == n) {
+        return mid;
+      }
+
+      if ((long) mid * mid < n) {
+        root = mid;        // store possible answer
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    // Precision part
+    double incr = 0.1;
+    for (int i = 0; i < p; i++) {
+      while (root * root <= n) {
+        root += incr;
+      }
+      root -= incr;
+      incr /= 10;
+    }
+
+    return root;
+  }
+}
